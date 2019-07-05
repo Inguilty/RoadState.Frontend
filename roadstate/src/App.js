74f1780888db2d49a35551a0020cf313061e1409
Yurii Pohuliaiev <yurii.pohuliaiev@nure.ua>
2019-07-05 14:49:19 +0300

    Firstly pushed React.js application with routing and 404Page

---
+++ roadstate/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Route, Switch } from "react-router-dom";
+import HomePage from "./components/home/HomePage";
+import AboutPage from "./components/about/AboutPage";
+import PageNotFound from "./PageNotFound";
+import Header from "./components/templates/Header";
+
+const App = () => (
+  <div class="container-fluid">
+    <Header />
+    <Switch>
+      <Route exact path="/" component={HomePage} />
+      <Route path="/about" component={AboutPage} />
+      <Route component={PageNotFound} />
+    </Switch>
+  </div>
+);
+
+export default App;
---
+++ roadstate/src/components/home/HomePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const HomePage = () => (
+  <div class="jumbotron">
+    <h1>Test Home</h1>
+    <Link to="about" className="btn btn-primary btn-lg">
+      Tets linking
+    </Link>
+  </div>
+);
+
+export default HomePage;

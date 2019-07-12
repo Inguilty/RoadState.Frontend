5f256b9b40f9c55b9775c4afc84517a3748d39d0
Alexander Izmiestiev <a.izmiestiev@gmail.com>
2019-07-12 13:38:27 +0000

    Merged PR 65: Created authorization and profile markup
    Sign in and Sign up page is created
    Markup of authorized and unauthorized user is created
    Log out part of markup is created
    Profile view is created
    
    Related work items: #69

--- roadstate/src/App.js
+++ roadstate/src/App.js
@@ -5,6 +5,10 @@ import HomePage from "./components/pages/home";
 import AboutPage from "./components/pages/about";
 import PageNotFound from "./components/pages/pagenotfound";
 import TestForm from "./components/pages/test";
+import SignIn from "./components/pages/authorization/SignIn";
+import SignUp from "./components/pages/authorization/SignUp";
+import LogOut from "./components/pages/authorization/LogOut";
+import ShowProfile from "./components/pages/authorization/ShowProfile";
 
 const App = props => (
   <div class="container-fluid">
@@ -14,6 +18,10 @@ const App = props => (
         <Route exact path="/" component={HomePage} />
         <Route path="/about" component={AboutPage} />
         <Route path="/test" component={TestForm} />
+        <Route path="/signIn" component={SignIn} />
+        <Route path="/signUp" component={SignUp} />
+        <Route path="/logOut" component={LogOut} />
+        <Route path="/profile" component={ShowProfile} />
         <Route component={PageNotFound} />
       </Switch>
     </div>

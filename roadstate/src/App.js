79b92647f0b8a4c45f3bfc2884fec26d0081f922
Максим <kuzmich.maxim.98@gmail.com>
2019-07-16 16:35:48 +0300

    itinerary

--- roadstate/src/App.js
+++ roadstate/src/App.js
@@ -1,30 +1,32 @@
-import React from "react";
-import Header from "./components/layout/Header";
-import { Route, Switch } from "react-router-dom";
-import HomePage from "./components/pages/home";
-import AboutPage from "./components/pages/about";
-import PageNotFound from "./components/pages/pagenotfound";
-import TestForm from "./components/pages/test";
-import SignIn from "./components/pages/authorization/SignIn";
-import SignUp from "./components/pages/authorization/SignUp";
-import LogOut from "./components/pages/authorization/LogOut";
-import ShowProfile from "./components/pages/authorization/ShowProfile";
+import React from 'react';
+import Header from './components/layout/Header';
+import { Route, Switch } from 'react-router-dom';
+import HomePage from './components/pages/home';
+import AboutPage from './components/pages/about';
+import PageNotFound from './components/pages/pagenotfound';
+import TestForm from './components/pages/test';
+import SignIn from './components/pages/authorization/SignIn';
+import SignUp from './components/pages/authorization/SignUp';
+import LogOut from './components/pages/authorization/LogOut';
+import ShowProfile from './components/pages/authorization/ShowProfile';
+import ViewMap from './components/viewmap/ViewMap';
 
 const App = props => (
-  <div class="container-fluid">
+  <div class='container-fluid'>
     <Header />
-    <div className="jumbotron">
+    <div className='jumbotron'>
       <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/about" component={AboutPage} />
-        <Route path="/test" component={TestForm} />
-        <Route path="/signIn" component={SignIn} />
-        <Route path="/signUp" component={SignUp} />
-        <Route path="/logOut" component={LogOut} />
-        <Route path="/profile" component={ShowProfile} />
+        <Route exact path='/' component={HomePage} />
+        <Route path='/about' component={AboutPage} />
+        <Route path='/test' component={TestForm} />
+        <Route path='/signIn' component={SignIn} />
+        <Route path='/signUp' component={SignUp} />
+        <Route path='/logOut' component={LogOut} />
+        <Route path='/profile' component={ShowProfile} />
         <Route component={PageNotFound} />
       </Switch>
     </div>
+    <ViewMap />
   </div>
 );
 

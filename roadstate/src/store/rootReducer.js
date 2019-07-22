b8e212e73b098c92a8f422cbc9ed8012ae9b4e23
Zhuravlov Dmitriy <box17776dima@gmail.com>
2019-07-22 15:49:37 +0300

    fix to accept run build

--- roadstate/src/store/rootReducer.js
+++ roadstate/src/store/rootReducer.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import bugReport from '../components/pages/bugreport/reducers';
-import user from '../components/pages/user/reducers';
 
 const rootReducer = combineReducers({
   bugReport,
-  user,
 });
 
 export default rootReducer;

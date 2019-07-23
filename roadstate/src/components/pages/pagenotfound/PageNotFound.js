157aa4b6a8d418a8cae4ceedd3cafc2745fe5d29
oleksandr.izmiestiev <oleksandr.izmiestiev@TRN08>
2019-07-23 19:06:29 +0300

    Eslint fixes.

--- roadstate/src/components/pages/pagenotfound/PageNotFound.js
+++ roadstate/src/components/pages/pagenotfound/PageNotFound.js
@@ -2,19 +2,17 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const PageNotFound = () => (
-  <div className='row'>
-    <div className='col-md-12'>
-      <div className='error-template'>
+  <div className="row">
+    <div className="col-md-12">
+      <div className="error-template">
         <h1>Oops!</h1>
         <h2>Page not found exception</h2>
-        <div className='error-details'>
+        <div className="error-details">
           An error has been occured. The requested page is not found!
         </div>
-        <div className='error-actions'>
-          <NavLink className='btn btn-lg' to='/' exact>
-            <span className='glyphicon glyphicon-home'>
-              Return to home page
-            </span>
+        <div className="error-actions">
+          <NavLink className="btn btn-lg" to="/" exact>
+            <span className="glyphicon glyphicon-home">Return to home page</span>
           </NavLink>
         </div>
       </div>
